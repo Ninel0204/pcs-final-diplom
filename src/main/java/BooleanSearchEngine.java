fc491837001219f22a3a1f093b83e68aff420d98
Ninel0204 <116366686+Ninel0204@users.noreply.github.com>
2023-03-06 19:03:55 +0300

    Add files via upload

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -4,32 +4,26 @@ import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class BooleanSearchEngine implements SearchEngine {
-    protected Map<String, List<PageEntry>> indexing;
+    protected Map<String, List<PageEntry>> indexing = new HashMap<>();
 
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
 
-        List<File> files;
-        try (Stream<Path> paths = Files.walk(Paths.get(String.valueOf(pdfsDir)))) {
-            files = paths
-                    .filter(Files::isRegularFile)
-                    .map(Path::toFile)
-                    .collect(Collectors.toList());
+        File[] listPdfFiles = getListFiles(pdfsDir, ".pdf");
+        if (listPdfFiles != null) {
+            for (File pdfFile : listPdfFiles) {
+                scan(pdfFile);
+            }
+            indexing.values().forEach(Collections::sort);
         }
+    }
 
-        indexing = new HashMap<>();
 
-        for (File file : files) {
-            var doc = new PdfDocument(new PdfReader(file));
-
+    private void scan(File pdfFile) throws IOException {
+        try (var doc = new PdfDocument(new PdfReader(pdfFile))) {
             for (int i = 1; i < doc.getNumberOfPages() + 1; i++) {
                 var pdfPage = doc.getPage(i);
                 var text = PdfTextExtractor.getTextFromPage(pdfPage);
@@ -43,7 +37,7 @@ public class BooleanSearchEngine implements SearchEngine {
                     word = word.toLowerCase();
                     freqs.put(word, freqs.getOrDefault(word, 0) + 1);
                 }
-           
+
 
                 for (Map.Entry<String, Integer> entry : freqs.entrySet()) {
                     List<PageEntry> pageEntries;
@@ -54,19 +48,36 @@ public class BooleanSearchEngine implements SearchEngine {
                     } else {
                         pageEntries = new ArrayList<>();
                     }
-                    pageEntries.add(new PageEntry(file.getName(), i, value));
+                    pageEntries.add(new PageEntry(pdfFile.getName(), i, value));
                     indexing.put(key, pageEntries);
+
                 }
             }
-            }
         }
+    }
 
 
     @Override
     public List<PageEntry> search(String word) {
         List<PageEntry> list = indexing.get(word.toLowerCase());
-        Collections.sort(list);
-        return list;
+        return Objects.requireNonNullElse(list, Collections.emptyList());
 
+    }
+
+    private File[] getListFiles(File pdfsDir, String typeOfFile) {
+        File[] tempListFiles = pdfsDir.listFiles();
+        if (tempListFiles == null) return null;
+        List<File> files = new ArrayList<>();
+        for (File file : tempListFiles) {
+            if (file.isDirectory()) {
+                File[] listFiles = getListFiles(file, typeOfFile);
+                if (listFiles != null) files.addAll(List.of(listFiles));
+            } else {
+                if (file.getName().toLowerCase().endsWith(typeOfFile)) {
+                    files.add(file);
+                }
+            }
+        }
+        return files.toArray(new File[0]);
     }
 }
